453c40a39762da2928a9a2681bec8d8bb3789ff3
hhc27 <hhc27@sfu.ca>
2024-04-17 23:54:35 -0700

    Final version
    finished website

---
+++ script.js
@@ -0,0 +1,14 @@
+function toRabbitat() { 
+            window.location.href = "rabbitats.html";
+        } 
+function toMacaron() { 
+            window.location.href = "macaron.html";
+        } 
+
+function toRabbitatProject() { 
+            window.location.href = "https://www.figma.com/proto/OYqhVFOScMYtIJVII5JH9Z/IAT-235---P4?page-id=23%3A2&type=design&node-id=742-256&viewport=473%2C268%2C0.02&t=sOBE6Pxd1Rep1bsK-1&scaling=scale-down&starting-point-node-id=742%3A256&show-proto-sidebar=1&mode=design";
+        } 
+
+function toMacaronProject() { 
+            window.location.href = "https://github.com/siimka113/PO2";
+        } 
