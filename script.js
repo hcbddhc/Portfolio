b13448be9826dd922b806013d1cabed50778bea9
hhc27 <hhc27@sfu.ca>
2025-01-09 04:20:00 -0800

    Adjusted rabbitats page based on feedback

--- script.js
+++ script.js
@@ -6,9 +6,13 @@ function toMacaron() {
             window.location.href = "macaron.html";
         } 
 
-function toRabbitatProject() { 
-            window.location.href = "https://www.figma.com/proto/OYqhVFOScMYtIJVII5JH9Z/IAT-235---P4?page-id=23%3A2&type=design&node-id=742-256&viewport=473%2C268%2C0.02&t=sOBE6Pxd1Rep1bsK-1&scaling=scale-down&starting-point-node-id=742%3A256&show-proto-sidebar=1&mode=design";
+function toRabbitatWebPrototype() { 
+    window.open("https://www.figma.com/proto/OYqhVFOScMYtIJVII5JH9Z/IAT-235---P4?page-id=23%3A2&node-id=709-466&p=f&viewport=-98%2C258%2C0.06&t=EuHdoU8uq3pbUkS1-1&scaling=scale-down-width&content-scaling=fixed&starting-point-node-id=742%3A256");
         } 
+
+function toRabbitatMobilePrototype() { 
+    window.open( "https://www.figma.com/proto/OYqhVFOScMYtIJVII5JH9Z/IAT-235---P4?page-id=23%3A2&node-id=710-647&p=f&viewport=-98%2C258%2C0.06&t=EuHdoU8uq3pbUkS1-1&scaling=scale-down-width&content-scaling=fixed&starting-point-node-id=742%3A256");
+} 
 
 function toMacaronProject() { 
             window.location.href = "https://github.com/siimka113/PO2";
