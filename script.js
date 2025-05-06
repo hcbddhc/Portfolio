98c8d19462bc140724e47fb2779d56ea159591ac
hhc27 <hhc27@sfu.ca>
2025-05-06 14:05:02 -0700

    changed links

--- script.js
+++ script.js
@@ -16,16 +16,20 @@ function toMacaron() {
 } 
 
 function toRabbitatWebPrototype() { 
-    window.open("https://www.figma.com/proto/OYqhVFOScMYtIJVII5JH9Z/IAT-235---P4?page-id=23%3A2&node-id=709-466&p=f&viewport=-98%2C258%2C0.06&t=EuHdoU8uq3pbUkS1-1&scaling=scale-down-width&content-scaling=fixed&starting-point-node-id=742%3A256");
-        } 
+    window.open("https://www.figma.com/proto/OYqhVFOScMYtIJVII5JH9Z/IAT-235---P4?page-id=23%3A2&node-id=709-466&p=f&viewport=-98%2C258%2C0.06&t=EuHdoU8uq3pbUkS1-1&scaling=scale-down-width&content-scaling=fixed&starting-point-node-id=742%3A256", "_blank");
+} 
 
 function toRabbitatMobilePrototype() { 
-    window.open( "https://www.figma.com/proto/OYqhVFOScMYtIJVII5JH9Z/IAT-235---P4?page-id=23%3A2&node-id=710-647&p=f&viewport=-98%2C258%2C0.06&t=EuHdoU8uq3pbUkS1-1&scaling=scale-down-width&content-scaling=fixed&starting-point-node-id=742%3A256");
+    window.open("https://www.figma.com/proto/OYqhVFOScMYtIJVII5JH9Z/IAT-235---P4?page-id=23%3A2&node-id=710-647&p=f&viewport=-98%2C258%2C0.06&t=EuHdoU8uq3pbUkS1-1&scaling=scale-down-width&content-scaling=fixed&starting-point-node-id=742%3A256", "_blank");
 } 
 
 function toMacaronProject() { 
-            window.location.href = "https://github.com/siimka113/PO2";
-        } 
+    window.open("https://github.com/siimka113/PO2", "_blank");
+} 
+
+function toCraftconnectPrototype() {
+    window.open("https://www.figma.com/proto/0seKlcqLSc2nzigScHuAab/IAT-333-Project-Ideation?page-id=1520%3A1241&node-id=1524-876&p=f&viewport=466%2C262%2C0.06&t=dPg8SEiSoPTVALk4-1&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=1524%3A876&show-proto-sidebar=1", "_blank");
+}
 
 //for navigation bar scroll color change
 window.onscroll = function() {
